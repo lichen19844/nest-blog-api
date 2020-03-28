8b66bdb1a17348502e291c0417640dd50dd30963
lichen <119449234@qq.com>
2020-03-28 19:31:03 +0800

    crud package

--- src/posts/post.model.ts
+++ src/posts/post.model.ts
@@ -1,9 +1,14 @@
 // import { prop, getModelForClass } from '@typegoose/typegoose';
 import { prop } from '@typegoose/typegoose';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
 
 export class Post {
+  // DTO的形式ApiProperty
+  @ApiProperty({ description: '帖子标题', example: '帖子标题1' })
   @prop()
   title: string
+  
+  @ApiProperty({ description: '帖子内容', example: '帖子内容1' })
   @prop()
   content: string
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,6 +6,7 @@ import { IsNotEmpty } from 'class-validator'
 import { InjectModel } from 'nestjs-typegoose';
 import { Post as PostSchema } from './post.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
+import { Crud } from 'nestjs-mongoose-crud';
 
 // 定义DTO数据传输对象
 // 为了标识创建帖子的数据，是一种规范
@@ -16,74 +17,98 @@ class CreatePostDto {
   @ApiProperty({ description: '帖子内容', example: '帖子内容1' })
   content: string
 }
-// 为了标识更新帖子的数据，是一种规范
-class UpdatePostDto {
-  @ApiProperty({ description: '帖子标题' })
-  title: string
-  @ApiProperty({ description: '帖子内容' })
-  content: string
-}
+// // 为了标识更新帖子的数据，是一种规范
+// class UpdatePostDto {
+//   @ApiProperty({ description: '帖子标题' })
+//   title: string
+//   @ApiProperty({ description: '帖子内容' })
+//   content: string
+// }
 
+@Crud({
+  model: PostSchema,
+  routes: {
+    find: {
+      decorators: [
+        ApiOperation({summary: '帖子列表'})
+      ]
+    },
+    create: {
+      dto: CreatePostDto,
+      decorators: [
+        ApiOperation({summary: '创建帖子'})
+      ]
+    },
+    findOne: {
+      decorators: [
+        ApiOperation({summary: '编辑帖子'})
+      ]
+    }
+  }
+})
 // 控制器路由
 @Controller('posts')
 @ApiTags('帖子')
 export class PostsController {
   constructor(
-    @InjectModel(PostSchema) private readonly PostModel: ModelType<PostSchema>
+    // @InjectModel(PostSchema) private readonly PostModel: ModelType<PostSchema>
+    @InjectModel(PostSchema) private readonly model: ModelType<PostSchema>
   ){}
-  @Get()  // 也可以写成@Get('/')
-  @ApiOperation({ summary: '显示帖子列表' })
-  async index() {
-    return await this.PostModel.find()
-    // return [
-    //   {id: 1},
-    //   {id: 2},
-    //   {id: 3},
-    //   {id: 4}
-    // ]
-  }
 
-  @Post()
-  @ApiOperation({ summary: '创建帖子' })
-  // create (@Body() body, @Query() query, @Param() params) {
-  async create(@Body() createPostDto: CreatePostDto) {
-    // body
-    // query
-    // params
-    await this.PostModel.create(createPostDto)
-    return {
-      success: true
-    }
-    // return createPostDto
-  }
+  // @Get()  // 也可以写成@Get('/')
+  // @ApiOperation({ summary: '显示帖子列表' })
+  // async index() {
+  //   return await this.PostModel.find()
+  //   // return [
+  //   //   {id: 1},
+  //   //   {id: 2},
+  //   //   {id: 3},
+  //   //   {id: 4}
+  //   // ]
+  // }
 
-  // 详情页
-  @Get(':id')
-  @ApiOperation({ summary: '帖子详情接口' })
-  async detail(@Param('id') id: string) {
-    return await this.PostModel.findById(id)
-    // return {
-    //   id,
-    //   title: "aaaa"
-    // }
-  }
+  // @Post()
+  // @ApiOperation({ summary: '创建帖子' })
+  // // create (@Body() body, @Query() query, @Param() params) {
+  // // async create(@Body() createPostDto: PostSchema) {
+  // async create(@Body() createPostDto: CreatePostDto) {
+  //   // body
+  //   // query
+  //   // params
+  //   await this.PostModel.create(createPostDto)
+  //   return {
+  //     success: true
+  //   }
+  //   // return createPostDto
+  // }
 
-  // 修改
-  @Put(':id')
-  @ApiOperation({ summary: '编辑帖子' })
-  async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    await this.PostModel.findByIdAndUpdate(id, updatePostDto)
-    return {
-      success: true
-    }
-  }
+  // // 详情页
+  // @Get(':id')
+  // @ApiOperation({ summary: '帖子详情接口' })
+  // async detail(@Param('id') id: string) {
+  //   return await this.PostModel.findById(id)
+  //   // return {
+  //   //   id,
+  //   //   title: "aaaa"
+  //   // }
+  // }
 
-  @Delete(':id')
-  @ApiOperation({ summary: '删除帖子' })
-  async remove(@Param('id') id: string) {
-    await this.PostModel.findByIdAndDelete(id)
-    return {
-      success: true
-    }
-  }
+  // // 修改
+  // @Put(':id')
+  // @ApiOperation({ summary: '编辑帖子' })
+  // async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+  //   await this.PostModel.findByIdAndUpdate(id, updatePostDto)
+  //   return {
+  //     success: true
+  //   }
+  // }
+
+  // @Delete(':id')
+  // @ApiOperation({ summary: '删除帖子' })
+  // async remove(@Param('id') id: string) {
+  //   await this.PostModel.findByIdAndDelete(id)
+  //   return {
+  //     success: true
+  //   }
+  // }
 }
